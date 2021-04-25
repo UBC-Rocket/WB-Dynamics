d5f3b49bbbb92657494b6e0894d7587412dff17d
BigBlue <shenpaiwilliam@gmail.com>
2021-04-25 09:13:54 -0700

    Change Tests To Use Relative Tolerance

--- tests/f_g_test.m
+++ tests/f_g_test.m
@@ -1,11 +1,18 @@
 %%  Unit tests for the `f_g` function to compute gravitational force
 %   Essentially the tests will try out a couple of different altitudes
 %   with a couple different masses at each altitude. Note that since we are
-%   comparing floating point values, we just compare to 5 significant
-%   figures instead of a strict equality.
+%   comparing floating point values, we just compare relative tolerances 
+%   instead of a strict equality.
 
 function tests = f_g_test
     tests = functiontests(localfunctions);
+end
+
+function verify(testCase, expected, actual)
+    tolerance = 1e-5;
+    verifyEqual(testCase, actual(1), 0);
+    verifyEqual(testCase, actual(2), expected, 'RelTol', tolerance);
+    verifyEqual(testCase, actual(3), 0);
 end
 
 function test_0km_small_mass(testCase)
@@ -13,9 +20,7 @@ function test_0km_small_mass(testCase)
     h = 0;
     expected = -0.098175;
     f_grav = f_g(m, h);
-    verifyEqual(testCase, f_grav(1), 0);
-    verifyEqual(testCase, round(f_grav(2), 5, 'significant'), expected);
-    verifyEqual(testCase, f_grav(3), 0);
+    verify(testCase, expected, f_grav);
 end
 
 function test_0km_unit_mass(testCase)
@@ -23,9 +28,7 @@ function test_0km_unit_mass(testCase)
     h = 0;
     expected = -9.8175;
     f_grav = f_g(m, h);
-    verifyEqual(testCase, f_grav(1), 0);
-    verifyEqual(testCase, round(f_grav(2), 5, 'significant'), expected);
-    verifyEqual(testCase, f_grav(3), 0);
+    verify(testCase, expected, f_grav);
 end
 
 function test_0km_big_mass(testCase)
@@ -33,9 +36,7 @@ function test_0km_big_mass(testCase)
     h = 0;
     expected = -981.75;
     f_grav = f_g(m, h);
-    verifyEqual(testCase, f_grav(1), 0);
-    verifyEqual(testCase, round(f_grav(2), 5, 'significant'), expected);
-    verifyEqual(testCase, f_grav(3), 0);
+    verify(testCase, expected, f_grav);
 end
 
 function test_50km_small_mass(testCase)
@@ -43,9 +44,7 @@ function test_50km_small_mass(testCase)
     h = 50000;
     expected = -0.096652;
     f_grav = f_g(m, h);
-    verifyEqual(testCase, f_grav(1), 0);
-    verifyEqual(testCase, round(f_grav(2), 5, 'significant'), expected);
-    verifyEqual(testCase, f_grav(3), 0);
+    verify(testCase, expected, f_grav);
 end
 
 function test_50km_unit_mass(testCase)
@@ -53,9 +52,7 @@ function test_50km_unit_mass(testCase)
     h = 50000;
     expected = -9.6652;
     f_grav = f_g(m, h);
-    verifyEqual(testCase, f_grav(1), 0);
-    verifyEqual(testCase, round(f_grav(2), 5, 'significant'), expected);
-    verifyEqual(testCase, f_grav(3), 0);
+    verify(testCase, expected, f_grav);
 end
 
 function test_50km_big_mass(testCase)
@@ -63,9 +60,7 @@ function test_50km_big_mass(testCase)
     h = 50000;
     expected = -966.52;
     f_grav = f_g(m, h);
-    verifyEqual(testCase, f_grav(1), 0);
-    verifyEqual(testCase, round(f_grav(2), 5, 'significant'), expected);
-    verifyEqual(testCase, f_grav(3), 0);
+    verify(testCase, expected, f_grav);
 end
 
 function test_100km_small_mass(testCase)
@@ -73,9 +68,7 @@ function test_100km_small_mass(testCase)
     h = 100000;
     expected = -0.095164;
     f_grav = f_g(m, h);
-    verifyEqual(testCase, f_grav(1), 0);
-    verifyEqual(testCase, round(f_grav(2), 5, 'significant'), expected);
-    verifyEqual(testCase, f_grav(3), 0);
+    verify(testCase, expected, f_grav);
 end
 
 function test_100km_unit_mass(testCase)
@@ -83,9 +76,7 @@ function test_100km_unit_mass(testCase)
     h = 100000;
     expected = -9.5164;
     f_grav = f_g(m, h);
-    verifyEqual(testCase, f_grav(1), 0);
-    verifyEqual(testCase, round(f_grav(2), 5, 'significant'), expected);
-    verifyEqual(testCase, f_grav(3), 0);
+    verify(testCase, expected, f_grav);
 end
 
 function test_100km_big_mass(testCase)
@@ -93,9 +84,7 @@ function test_100km_big_mass(testCase)
     h = 100000;
     expected = -951.64;
     f_grav = f_g(m, h);
-    verifyEqual(testCase, f_grav(1), 0);
-    verifyEqual(testCase, round(f_grav(2), 5, 'significant'), expected);
-    verifyEqual(testCase, f_grav(3), 0);
+    verify(testCase, expected, f_grav);
 end
 
 function test_150km_small_mass(testCase)
@@ -103,9 +92,7 @@ function test_150km_small_mass(testCase)
     h = 150000;
     expected = -0.093710;
     f_grav = f_g(m, h);
-    verifyEqual(testCase, f_grav(1), 0);
-    verifyEqual(testCase, round(f_grav(2), 5, 'significant'), expected);
-    verifyEqual(testCase, f_grav(3), 0);
+    verify(testCase, expected, f_grav);
 end
 
 function test_150km_unit_mass(testCase)
@@ -113,9 +100,7 @@ function test_150km_unit_mass(testCase)
     h = 150000;
     expected = -9.3710;
     f_grav = f_g(m, h);
-    verifyEqual(testCase, f_grav(1), 0);
-    verifyEqual(testCase, round(f_grav(2), 5, 'significant'), expected);
-    verifyEqual(testCase, f_grav(3), 0);
+    verify(testCase, expected, f_grav);
 end
 
 function test_150km_big_mass(testCase)
@@ -123,9 +108,7 @@ function test_150km_big_mass(testCase)
     h = 150000;
     expected = -937.10;
     f_grav = f_g(m, h);
-    verifyEqual(testCase, f_grav(1), 0);
-    verifyEqual(testCase, round(f_grav(2), 5, 'significant'), expected);
-    verifyEqual(testCase, f_grav(3), 0);
+    verify(testCase, expected, f_grav);
 end
 
 
