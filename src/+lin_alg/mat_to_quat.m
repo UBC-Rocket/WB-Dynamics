8092213053faa99e475d0721fbfd8f88cf068e42
BigBlue <shenpaiwilliam@gmail.com>
2021-05-19 15:55:42 -0600

    Add Tests For Matrix To Quaternion Conversion

--- src/+lin_alg/mat_to_quat.m
+++ src/+lin_alg/mat_to_quat.m
@@ -11,36 +11,35 @@ function q = mat_to_quat(C)
 %           matrix
 %
 %   Source:
-%   Atmospheric and Space Flight Dynamics Modeling and Simulation with MATLAB 
-%   by Ashish Tewari
+%   http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion
 %
-    T = trace(C);
-    qsq = [1+2*C(1,1)-T; 1+2*C(2,2)-T; 1+2*C(3,3)-T; 1+T]/4;
-    [x,i] = max(qsq);
-    
-    if i == 4
-        q(4) = sqrt(x);
-        q(1) = (C(2,3)-C(3,2))/(4*q(4));
-        q(2) = (C(3,1)-C(1,3))/(4*q(4));
-        q(3)
+    tr = C(1,1) + C(2,2) + C(3,3);
+
+    if tr > 0 
+        S = sqrt(tr + 1) * 2; 
+        q_w = 0.25 * S;
+        q_x = (C(3,2) - C(2,3)) / S;
+        q_y = (C(1,3) - C(3,1)) / S; 
+        q_z = (C(2,1) - C(1,2)) / S; 
+    elseif (C(1,1) > C(2,2)) && (C(1,1) > C(3,3)) 
+        S = sqrt(1 + C(1,1) - C(2,2) - C(3,3)) * 2;
+        q_w = (C(3,2) - C(2,3)) / S;
+        q_x = 0.25 * S;
+        q_y = (C(1,2) + C(2,1)) / S; 
+        q_z = (C(1,3) + C(3,1)) / S; 
+    elseif C(2,2) > C(3,3)
+        S = sqrt(1 + C(2,2) - C(1,1) - C(3,3)) * 2;
+        q_w = (C(1,3) - C(3,1)) / S;
+        q_x = (C(1,2) + C(2,1)) / S; 
+        q_y = 0.25 * S;
+        q_z = (C(2,3) + C(3,2)) / S; 
+    else 
+        S = sqrt(1 + C(3,3) - C(1,1) - C(2,2)) * 2;
+        q_w = (C(2,1) - C(1,2)) / S;
+        q_x = (C(1,3) + C(3,1)) / S;
+        q_y = (C(2,3) + C(3,2)) / S;
+        q_z = 0.25 * S; 
     end
-    if i == 3
-        q(3) = sqrt(x);
-        q(1) = (C(1,3)+C(3,1))/(4*q(3));
-        q(2) = (C(3,2)+C(2,3))/(4*q(3));
-        q(4) = (C(1,2)-C(2,1))/(4*q(3));
-    end
-    if i == 2
-        q(2) = sqrt(x);
-        q(1) = (C(1,2)+C(2,1))/(4*q(2));
-        q(3) = (C(3,2)+C(2,3))/(4*q(2));
-        q(4) = (C(3,1)-C(1,3))/(4*q(2));
-    end
-    if i == 1
-        q(1) = sqrt(x);
-        q(2) = (C(1,2)+C(2,1))/(4*q(1));
-        q(3) = (C(1,3)+C(3,1))/(4*q(1));
-        q(4) = (C(2,3)-C(3,2))/(4*q(1));
-    end
+    q = [q_x; q_y; q_z; q_w];
 end
 
